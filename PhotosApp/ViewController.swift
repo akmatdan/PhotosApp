9158e30558e0d50ddb57b6f4d66a17db5357cd20
Dan Akmat <akmatdan@gmail.com>
2021-07-21 10:48:12 +0600

    Initial Commit

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Daniil Akmatov on 21/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
